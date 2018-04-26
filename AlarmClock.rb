b8f95660585c904c252303842df0b56415314f6e
Fadini <fadinid91@gmail.com>
2018-04-26 15:13:12 -0400

    Added 12hr support
    Initially only worked with 24hr time, now inputing a 12hr time works correctly.

--- AlarmClock.rb
+++ AlarmClock.rb
@@ -8,25 +8,33 @@ require 'date'
 
 #get user input
 puts "Alarm Clock"
+puts "24 or 12 hour time? (24)/(12)"
+timeHour = gets.chomp.to_i
+if timeHour == 12
+	puts "(1) AM/ (2) PM"
+	amPM = gets.chomp.to_i
+end
 puts "What hour should you be alerted?"
-h = gets.chomp
+hour = gets.chomp.to_i
 puts "How many minutes shall you be alerted?"
-m = gets.chomp
+min = gets.chomp.to_i
 
-x = h.to_i
-y = m.to_i
 i = 0
 
+if timeHour == 12 && amPM == 2
+	hour+=12
+end
+
 current_time = DateTime.now
-setTime = DateTime.new(2017,4,26, x, y)
+setTime = DateTime.new(2017,4,26, hour, min)
 
 current_time.strftime "%H:%M"
 setTime.strftime "%H:%M"
 
-if y < 10
-	puts "Alarm set for #{x}:0#{y}"
+if min < 10
+	puts "Alarm set for #{hour}:0#{min}"
 else
-	puts "Alarm set for #{x}:#{y}"
+	puts "Alarm set for #{hour}:#{min}"
 end
 
 while i < 1
